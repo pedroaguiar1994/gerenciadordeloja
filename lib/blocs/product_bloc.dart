020139a88b5599d7ea008391752602c472adb680
Pedro Henrique <aguiarhora@gmail.com>
2019-05-05 00:40:32 -0400

    Melhoria do aplicativo

--- lib/blocs/product_bloc.dart
+++ lib/blocs/product_bloc.dart
@@ -1,17 +1,31 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/subjects.dart';
 
 class ProductBloc extends BlocBase{
-  
+  final _dataController = BehaviorSubject<Map>();
+
+  Stream<Map> get outData => _dataController.stream; 
   String categoryId;
   DocumentSnapshot product;
 
+  Map<String, dynamic> unsavedData;
+
   ProductBloc({this.categoryId, this.product}){
-    
+    if(product != null){
+      unsavedData = Map.of(product.data);
+      unsavedData["images"] = List.of(product.data["images"]);
+      unsavedData["images"] = List.of(product.data["sizes"]);
+    }else {
+      unsavedData= {
+        "title": null, "description": null , "price": null, "images":[], "sizes":[]
+      };
+    }
+    _dataController.add(unsavedData);
   }
   @override
   void dispose() {
-
+    _dataController.close();
   }
 
 }
--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,20 +1,45 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:gerente_loja/blocs/product_bloc.dart';
+import 'package:gerente_loja/widgets/images_widget.dart';
 
-class ProductScreen extends StatelessWidget {
+class ProductScreen extends StatefulWidget {
   
   final String categoryId;
   final DocumentSnapshot product;
 
+
+
+  ProductScreen({this.categoryId, this.product}) ;
+      
+
+  @override
+  _ProductScreenState createState() => _ProductScreenState(categoryId, product);
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+
   final ProductBloc _productBloc;
+
   final _formkey = GlobalKey<FormState>();
 
-  ProductScreen({this.categoryId, this.product}) :
-      _productBloc = ProductBloc(categoryId:  categoryId, product: product);
+  _ProductScreenState(String categoryId, DocumentSnapshot product):
+          _productBloc = ProductBloc(categoryId: categoryId, product: product);
 
   @override
   Widget build(BuildContext context) {
+    InputDecoration _buildDecoration(String label){
+      return InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.grey)
+      );
+    }
+
+    final _fieldStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 16
+    );
+
     return Scaffold(
       backgroundColor: Colors.grey[850],
       appBar: AppBar(
@@ -33,11 +58,52 @@ class ProductScreen extends StatelessWidget {
       ),
       body: Form(
         key: _formkey,
-        child: ListView(
-          padding: EdgeInsets.all(16),
-          children: <Widget>[
-            
-          ],
+        child: StreamBuilder<Map>(
+          stream: _productBloc.outData,
+          builder: (context, snapshot) {
+            if(!snapshot.hasData) return Container();
+            return ListView(
+              padding: EdgeInsets.all(16),
+              children: <Widget>[
+                Text
+                  ("Imagens",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16
+                ),
+                ),
+                ImagesWidget(
+                  context:context,
+                  initialValue: snapshot.data["images"],
+                  onSaved: (l){},
+                  validator:(l){},
+                ),
+                TextFormField(
+                  initialValue:  snapshot.data["title"],
+                  style: _fieldStyle,
+                  decoration: _buildDecoration("Titulo"),
+                  onSaved: (t){},
+                  validator: (t){},
+                ),
+                TextFormField(
+                  initialValue:  snapshot.data["description"],
+                  style: _fieldStyle,
+                  maxLines: 6,
+                  decoration: _buildDecoration("Descriçao"),
+                  onSaved: (t){},
+                  validator: (t){},
+                ),
+                TextFormField(
+                  initialValue:  snapshot.data["price"]?.toStringAsFixed(2),
+                  style: _fieldStyle,
+                  decoration: _buildDecoration("Preco"),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  onSaved: (t){},
+                  validator: (t){},
+                )
+              ],
+            );
+          }
         ),
       ),
     );
